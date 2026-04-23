ce612dca99538ca38017605294b089b231ce5906
Ash Hines <ahines@MacBookPro.jarve.ahines.lan>
2026-04-23 23:38:37 +0300

    init

---
+++ random/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "~> 1.14.0"
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "http" {}
+}
+
+resource "random_pet" "name" {
+  length = 4
+}
+
+variable "length" {
+  type = number
+  default = 32
+}
+
+# Additional resource that depends on random_pet
+resource "random_password" "derived" {
+  length  = 32
+  special = true
+
+  # This forces dependency so your state viewer sees a relationship
+  keepers = {
+    pet_name = random_pet.name.id
+  }
+}
+
+output "pet_name" {
+  value = random_pet.name.id
+}
+
+# Normal output referencing dependent resource
+output "derived_password_preview" {
+  value = substr(random_password.derived.result, 0, 4)
+  sensitive = true
+}
